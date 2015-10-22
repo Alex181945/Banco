fbe0545d6ed9e3d7c651683f01ba51bed06ff6de
Alejandro Estrada <alestra55@hotmail.com>
2015-10-22 13:47:18 -0500

    OK

--- Banco/src/main/java/com/itgam/cachorros/modelo/Cliente.java
+++ Banco/src/main/java/com/itgam/cachorros/modelo/Cliente.java
@@ -1,5 +1,35 @@
 package com.itgam.cachorros.modelo;
 
 public class Cliente {
-
+	
+	private Integer iIdCliente;
+	private String cNombre;
+	private String cApellidos;
+	private Integer iNumCuenta;
+	
+	public Integer getiIdCliente() {
+		return iIdCliente;
+	}
+	public void setiIdCliente(Integer iIdCliente) {
+		this.iIdCliente = iIdCliente;
+	}
+	public String getcNombre() {
+		return cNombre;
+	}
+	public void setcNombre(String cNombre) {
+		this.cNombre = cNombre;
+	}
+	public String getcApellidos() {
+		return cApellidos;
+	}
+	public void setcApellidos(String cApellidos) {
+		this.cApellidos = cApellidos;
+	}
+	public Integer getiNumCuenta() {
+		return iNumCuenta;
+	}
+	public void setiNumCuenta(Integer iNumCuenta) {
+		this.iNumCuenta = iNumCuenta;
+	}
+	
 }
--- Banco/src/main/java/com/itgam/cachorros/modelo/Cuenta.java
+++ Banco/src/main/java/com/itgam/cachorros/modelo/Cuenta.java
@@ -2,4 +2,21 @@ package com.itgam.cachorros.modelo;
 
 public class Cuenta {
 
+	private Integer id_Cuenta;
+	private Integer isaldo;
+	
+	public Integer getId_Cuenta() {
+		return id_Cuenta;
+	}
+	public void setId_Cuenta(Integer id_Cuenta) {
+		this.id_Cuenta = id_Cuenta;
+	}
+	public Integer getIsaldo() {
+		return isaldo;
+	}
+	public void setIsaldo(Integer isaldo) {
+		this.isaldo = isaldo;
+	}
+	
+	
 }

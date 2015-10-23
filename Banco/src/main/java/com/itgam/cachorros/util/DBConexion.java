98313e6caf85d322a4d382100327356f8be5d2c0
alest <alest@Alejandro>
2015-10-22 23:15:06 -0500

    Fallo en conexion :S

--- Banco/src/main/java/com/itgam/cachorros/util/DBConexion.java
+++ Banco/src/main/java/com/itgam/cachorros/util/DBConexion.java
@@ -1,5 +1,23 @@
 package com.itgam.cachorros.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConexion {
 
+	public static Connection GetConnection(){
+		
+		Connection conexion = null;
+		
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://localhost;"
+            		+ "databaseName=PRUEBA_TRANS;user=ALEJANDRO\\alest;password=HCM_1942_esiqie_;";
+            conexion= DriverManager.getConnection(url);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error en la conexion: "+e);
+		}
+		return conexion;
+	}
 }
---
+++ Banco/src/main/java/com/itgam/cachorros/util/Prueba.java
@@ -0,0 +1,11 @@
+package com.itgam.cachorros.util;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DBConexion prueba = new DBConexion();
+		prueba.GetConnection();
+	}
+
+}

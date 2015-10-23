f2d36805ddae62d10a73e032c1c8727eac54348c
alest <alest@Alejandro>
2015-10-22 23:37:49 -0500

    Ya conecta a la base de datos

--- Banco/src/main/java/com/itgam/cachorros/util/DBConexion.java
+++ Banco/src/main/java/com/itgam/cachorros/util/DBConexion.java
@@ -12,7 +12,8 @@ public class DBConexion {
 		
 		try {
             Class.forName("com.mysql.jdbc.Driver");
-            conexion = DriverManager.getConnection("jdbc:mysql://localhost/materia","root","root");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3311/materia","root","root");
+            System.out.println("Me conecte exitosamente");
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Error de Conexión Intente Mas Tarde"
                     + " O Contacté con el área de Sistemas"+"\n"+e);
